952bf3d9b49a866f1fae4517670833c4fe5ab84f
thanapoom128 <144669607+thanapoom128@users.noreply.github.com>
2024-11-24 20:18:50 +0700

    Update WelcomeScreen.tsx

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function WelcomeScreen({ navigation }: any) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Mobile Airline App</Text>
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+
+      <Image source={require("../../assets/AirMe.png")} style={styles.logo} />
+
+   
+      <Text style={styles.title}>Welcome to AirMe</Text>
+      
+     
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,10 +26,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#f9f9f9",
+    padding: 20,
+  },
+  logo: {
+    width: 150, 
+    height: 150, 
+    resizeMode: "contain",
+    marginBottom: 30,
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: "bold",
-    marginBottom: 20,
+    color: "#007bff",
+    marginBottom: 40,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
